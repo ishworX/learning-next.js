0eee04d8d66fbb84ea73bccff61853f103f93bd5
ishworX <git.ishwor@gmail.com>
2023-10-11 01:26:56 -0600

    Working on the hero section

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> Portfolio Website </h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSection />
     </main>
   )
 }
