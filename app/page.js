4accb317f16380a91b07c0a109ec6fa8883a909d
ishworX <git.ishwor@gmail.com>
2024-05-07 23:37:23 -0600

    Its working

--- app/page.js
+++ app/page.js
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Check !!! 
-
-      Happy Birthday to me! Day 3
-      // I am just slacking now
-
+      Home
     </main>
   );
 }
