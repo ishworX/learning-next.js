5d43cc78edfd2c6c7fad7441df7ac280200a56ff
ishworX <git.ishwor@gmail.com>
2024-05-16 23:36:20 -0600

    test?

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+      <Header />
+      {children}
+      </body>
     </html>
   );
 }
