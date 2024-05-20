1dba403f790b3d6c82916c9e1be3b059af8769a9
ishworX <git.ishwor@gmail.com>
2024-05-20 04:07:12 -0600

    Fixed the error on scrolling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Logo from './Logo'
 import Nav from './Nav'
 import ThemeToggler from './ThemeToggler'
 import MobileNav from './MobileNav'
 
 const Header = () => {
+
+  const [header, setHeader] = useState(false);
+
+  useEffect (() => {
+    const scrollYpos = window.addEventListener('scroll', () => {
+      window.scroll > 50? setHeader(true) :setHeader(false);
+    });
+
+    return () => window.removeEventListener('scroll', scrollYpos);
+  });
+
   return (
     <header className='sticky top-0 py-4 z-30 transition-all max-padd-container flexBetween bg-[#fdf3fb] dark:bg-transparent'>
       {/* My logo goes here */}
