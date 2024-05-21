416a7d1f78548eeaff80be91621b9f26ad3b7aae
ishworX <git.ishwor@gmail.com>
2024-05-21 02:51:35 -0600

    completed Nav Bar

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={'h-[2000px] ${nunito.className}'}>
+      <body className={nunito.className}>
       <Header/>
       {children}
       </body>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,32 @@
+import { LINKS } from '@/public/data'
+import { motion } from 'framer-motion'
+import { Link } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+
+  const path = usePathname();
+
   return (
-    <div>Nav</div>
+    <nav className={`${containerStyles}`}>
+      {LINKS.map((link, i) => (
+        <Link href={link.path} key={i} className={`${linkStyles}`}>
+          {link.path === path && (
+            <motion.span
+              initial={{ y: '-1000%' }}
+              animate={{ y: 0 }}
+              transition={{ type: 'tween' }}
+              layoutId='undrline'
+              className={`${underlineStyles}`}
+            >
+            </motion.span>)}
+            {link.title}
+        </Link>
+      ))}
+    </nav>
   )
-  
+
 }
 
 export default Nav
