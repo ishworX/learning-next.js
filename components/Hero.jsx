f981f16ac426b31a5fd72f624eeee2033bb3ee39
ishworX <git.ishwor@gmail.com>
2024-05-26 23:46:52 -0600

    initital commit for Hero sectin

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,7 @@ const Hero = () => {
   return (
     <div className=''>
       Hero</div>
-     
-     // Plans for the hero sectiom
-     // Get a portrait for the hero sdction 
-     // Finish setting up linkedIn
+
   )
 }
 
