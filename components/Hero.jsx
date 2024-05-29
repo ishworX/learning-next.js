91daf96b96421b2c0e3d68717cdae838555be722
ishworX <git.ishwor@gmail.com>
2024-05-28 23:54:05 -0600

    testing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import SocialIcons from '@/components/SocialIcons'
+import SocialIcons from '@/components/ui/SocialIcons'
 import Image from 'next/image'
 // Type writer
 import { Typewriter } from 'react-simple-typewriter'
 
 //framer motion
 import {motion} from 'framer-motion'
-import fadeIn from '@/components/Variants'
+import fadeIn from '@/components/ui/Variants'
 
 const Hero = () => {
   return (
