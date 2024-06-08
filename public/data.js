ca3a713f1fd07422a3cbc89c65d703d3b93dd493
ishworX <git.ishwor@gmail.com>
2024-06-07 19:28:28 -0600

    New data updated !

--- public/data.js
+++ public/data.js
@@ -1,232 +1,242 @@
-// Please check 17:29 
-import {
-  HiHome,
-  HiUser,
-  HiSquare2Stack,
-  HiRectangleGroup,
-  HiChatBubbleBottomCenterText,
-  HiEnvelope,
-} from "react-icons/hi2";
-
-import { RxDesktop } from "react-icons/rx";
-
-import { BiBug } from "react-icons/bi";
-
-import {
-  FaBug,
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaFigma,
-  FaNodeJs,
-} from "react-icons/fa";
-
-import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
+import { Paintbrush, Layers, FileJson } from "lucide-react";
 
 export const LINKS = [
   {
     title: "home",
-    icon: <HiHome />,
     path: "/",
-    offset: -50,
   },
   {
     title: "portfolio",
-    icon: <HiChatBubbleBottomCenterText />,
     path: "/portfolio",
-    offset: -50,
   },
   {
     title: "contact",
-    icon: <HiEnvelope />,
     path: "/contact",
-    offset: -50,
   },
 ];
 
-// ACCORDION SECTION
 export const ACCORDION = [
   {
-    question: "What sets your approach apart from others?",
+    question: "Who are you and what do you do?",
     answer:
-      "I prioritize a holistic approach, blending user experience with technical excellence to create functional and aesthetically pleasing websites.",
+      "I'm Steve Parker, a passionate developer with a keen interest in development.",
   },
   {
-    question: "How do you ensure client satisfaction?",
+    question: "What motivates you in your career?",
     answer:
-      "I prioritize active communication and collaboration, ensuring that client feedback is incorporated at every stage of the design and development process.",
+      "My passion, driving me to continually grow and excel in my career.",
   },
   {
-    question: "How you balance creativity in your designs?",
-    answer:
-      "I approach each project with a blend of innovation and practicality, leveraging my technical expertise to bring creative visions to life while ensuring optimal functionality.",
+    question: "Refund policy on cancellations?",
+    answer: "Refunds available per our policy.",
   },
   {
-    question: "How do you balance creativity in your designs?",
+    question: "How do you approach problem-solving?",
     answer:
-      "I balance creativity by blending innovation with practicality, utilizing technical expertise to realize creative visions while ensuring functionality and performance.",
+      "I tackle problems methodically, breaking them down into smaller tasks.",
+  },
+  {
+    question: "What technologies are you proficient in?",
+    answer: "I'm proficient in JavaScript, HTML, CSS, and various frameworks.",
+  },
+  {
+    question: "Can you describe your communication style?",
+    answer:
+      "I communicate clearly and openly, ensuring everyone is on the same page.",
   },
 ];
 
-//  About data
-export const aboutData = [
+export const EDUCATION = [
   {
-    title: "expertise",
-    info: [
-      {
-        title: "Web Development",
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-        ],
-      },
-      {
-        title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
+    title: "Full Stack Development - ABC University, USA",
+    year: "2011",
   },
   {
-    title: "experience",
-    info: [
-      {
-        title: "UX/UI Designer - XYZ Company",
-        year: "2012 - 2023",
-      },
-      {
-        title: "Web Developer - XYZ Company",
-        year: "2010 - 2012",
-      },
-      {
-        title: "Freelancer - Fiver",
-        year: "2008 - 2010",
-      },
-    ],
+    title: "FontEnd Development - ABC University, Ldondon",
+    year: "2009",
   },
   {
-    title: "education",
-    info: [
-      {
-        title: "Full Stack Development - ABC University, USA",
-        year: "2011",
-      },
-      {
-        title: "FontEnd Development - ABC University, Ldondon",
-        year: "2009",
-      },
-      {
-        title: "Graphic Designing - ABC University, Turkey",
-        year: "2006",
-      },
-    ],
+    title: "Graphic Designing - ABC University, Turkey",
+    year: "2006",
+  },
+];
+export const EXPERINCE = [
+  {
+    title: "UX/UI Designer - XYZ Company",
+    year: "2012 - 2023",
+  },
+  {
+    title: "Web Developer - XYZ Company",
+    year: "2010 - 2012",
+  },
+];
+
+// SERVICES SECTION
+export const SERVICES = [
+  {
+    icon: <Paintbrush size={44} strokeWidth={0.8} />,
+    title: "FrontEnd Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
+  },
+  {
+    icon: <FileJson size={44} strokeWidth={0.8} />,
+    title: "BackEnd Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
+  },
+  {
+    icon: <Layers size={44} strokeWidth={0.8} />,
+    title: "FullStack Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
   },
 ];
 
 // Work data
-export const workData = [
+export const WORKDATA = [
   {
-    title: "FrontEnd",
-    projects: [
-      {
-        title: "Website",
-        url: "/img1.png",
-      },
-      {
-        title: "Website",
-        url: "/img2.png",
-      },
-      {
-        title: "Application",
-        url: "/img3.png",
-      },
-      {
-        title: "Application",
-        url: "/img4.png",
-      },
-    ],
+    title: "frontEnd Website",
+    url: "/img1.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
   },
   {
-    title: "backend",
-    projects: [
-      {
-        title: "Website",
-        url: "/img4.png",
-      },
-      {
-        title: "Website",
-        url: "/img3.png",
-      },
-      {
-        title: "Application",
-        url: "/img1.png",
-      },
-      {
-        title: "Application",
-        url: "/img5.png",
-      },
-    ],
+    title: "frontEnd Website",
+    url: "/img2.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
   },
   {
-    title: "Full Stack",
-    projects: [
-      {
-        title: "Website",
-        url: "/img5.png",
-      },
-      {
-        title: "Website",
-        url: "/img1.png",
-      },
-      {
-        title: "Application",
-        url: "/img2.png",
-      },
-      {
-        title: "Application",
-        url: "/img3.png",
-      },
-    ],
+    title: "frontEnd Application",
+    url: "/img3.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "frontEnd Application",
+    url: "/img4.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Website",
+    url: "/img4.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Website",
+    url: "/img3.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Application",
+    url: "/img1.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Application",
+    url: "/img5.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Website",
+    url: "/img5.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Website",
+    url: "/img1.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Application",
+    url: "/img2.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Application",
+    url: "/img3.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
   },
 ];
 
-//  Testimonial data
-export const testimonialData = [
+// TESTIMONIAL SECTION
+export const TESTIMONIAL = [
   {
-    url: "/person-1.jpg",
-    name: "Olivia Anderson",
-    position: "Customer",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+    title: "James Martinez",
+    profession: "Director, Nova Gold",
+    URL: "/person-1.jpg",
+    desc: "Fantastic destinations, knowledgeable guides, seamless logistics. Spectacular views and memories. Exceeded expectations; worth every penny",
   },
   {
-    url: "/person-2.jpg",
-    name: "Lucas Taylor",
-    position: "Customer",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+    title: "Sophia Anderson",
+    profession: "Manager, Safe City",
+    URL: "/person-2.jpg",
+    desc: "Unforgettable experiences, friendly staff, and top-notch organization. Highly recommended for adventurous souls. A must-try. Highly recommend",
   },
   {
-    url: "/person-3.jpg",
-    name: "Emma Brown",
-    position: "Customer",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+    title: "Lily Walker",
+    profession: "Director, High Gold",
+    URL: "/person-3.jpg",
+    desc: "Unbelievable sights, professional guides, seamless planning. Unmatched experiences that leave you wanting more. Highly recommended",
   },
   {
-    url: "/person-4.jpg",
-    name: "Sophia Martinez",
-    position: "Customer",
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+    title: "Ninna Williams",
+    profession: "CEO, Rafa Builds",
+    URL: "/person-4.jpg",
+    desc: "Unparalleled expertise, personalized service, breathtaking destinations. Consistently delivers an extraordinary travel experience.",
+  },
+  {
+    title: "Ali Khan",
+    profession: "CEO, High Chase",
+    URL: "/person-5.jpg",
+    desc: "Outstanding journeys, knowledgeable guides, seamless execution. Each trip exceeds expectations, ensuring lasting memories and remarkable experiences.",
   },
 ];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "About Us",
+      "Destinations",
+      "Travel Guides",
+      "Book Now",
+      "FAQ",
+      "Privacy Policy",
+    ],
+  },
+  {
+    title: "Our Community",
+    links: ["Terms and Conditions", "Special Offers", "Customer Reviews"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Contact Number", value: "123-456-7890" },
+    { label: "Email Address", value: "info@passport.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/linkedin.svg",
+  ],
+};
