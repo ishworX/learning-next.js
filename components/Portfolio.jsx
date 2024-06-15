b8b6b34d87ae4a5cc458e45a4062f4d81ff9c60d
ishworX <git.ishwor@gmail.com>
2024-06-14 23:55:51 -0600

    made changes on the Portfolio section

---
+++ components/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+
+const Portfolio = () => {
+    return (
+        <div>Portfolio</div>
+        
+        
+    )
+}
+
+export default Portfolio
